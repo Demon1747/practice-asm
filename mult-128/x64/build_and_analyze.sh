cce37019465954f504a9be0311a0b3c002bf5bea
Demon1747 <specdok117@mail.ru>
2024-10-06 20:32:21 +0300

    ASM x64 experiment data has been uploaded

--- mult-128/x64/build_and_analyze.sh
+++ mult-128/x64/build_and_analyze.sh
@@ -3,9 +3,9 @@
 # Build without optimisations
 nasm -f elf64 x64_mul128.asm -o x64_mul128.o
 gcc ../main-128.c x64_mul128.o -o x64_mult_128
-perf stat -r 10 -e user_time,instructions,cycles ./x64_mult_128
+perf stat -r 10 -o analize_data_x64_asm.txt -e user_time,instructions,cycles ./x64_mult_128
 
 # Build with optimisations
 gcc ../main-128.c x64_mul128.o -O3 -o x64_mult_128_o3
-perf stat -r 10 -e user_time,instructions,cycles ./x64_mult_128_o3
+perf stat -r 10 -o analize_data_x64_asm_o3.txt -e user_time,instructions,cycles ./x64_mult_128_o3
 
